d38ef7df7988abe840c77736596a671161fa7160
Monty Lennie <montylennie@gmail.com>
2015-04-06 14:53:55 +0200

    get user creation and index request handlers working

--- node/index.js
+++ node/index.js
@@ -8,8 +8,11 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 var IP = process.env.MONGODB_PORT_27017_TCP_ADDR
-var PORT = process.env.MONGODB_PORT_27017_TCP_PORT
-mongoose.connect('mongodb://' + IP + ':' + PORT + '/' + 'beauty-dev');
+var MONGOPORT = process.env.MONGODB_PORT_27017_TCP_PORT
+mongoose.connect('mongodb://' + IP + ':' + MONGOPORT + '/' + 'beauty-dev');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -20,37 +23,41 @@ db.once('open', function (callback) {
     email: String,
     passwordHash: String
 	});
-});
 
+	var User = mongoose.model('User', userSchema);
 
-var hashPassword = function (user, callback) {
-  bcrypt.hash(user.password, 10, function(error, hash) {
-    if (error) throw error;
-    user.password = hash;
-    callback(null, user);
-	}); 
-};
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-app.get('/', function( req, res) {
-  res.send('Hello yoda!!');
-});
-
-app.post('/users', function( req, res) {
-	bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(req.body.password, salt, function(err, hash) {
-    	bcrypt.compare(req.body.password, hash, function(err, response) {
-	    	var passwordEqualsHash = response == true;
-	    	res.json({hash: hash, comparison: passwordEqualsHash.toString()});
-			});
-    });
+	app.get('/', function( req, res) {
+	  res.send('Hello yoda!!');
 	});
-});
+
+	app.post('/users', function(req, res) {
+		bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(req.body.password, salt, function(err, hash) {
+	    	var user = new User({ 
+	    		username: req.body.username,
+	    		email: req.body.email,
+	    		passwordHash: hash 
+	    	})
+	    	user.save(function (err, user) {
+				  if (err) return console.error(err);
+				  console.log('user created: ' + user);
+				  res.send('user created: ' + user);
+				});
+	    });
+		});
+	});
+
+	app.get('/users', function(req, res) {
+		User.find(function (err, users) {
+		  if (err) return console.error(err);
+		  console.log(users);
+		  res.json(users);
+		});
+	});	
 
  
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+	app.listen(PORT);
+	console.log('Running on http://localhost:' + PORT);
+});
+
+
