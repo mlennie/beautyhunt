b89b644bf221a670266baf8d0d6ea4f24d8c59ee
Monty Lennie <montylennie@gmail.com>
2015-04-14 22:07:25 +0200

    create session mixin with current user

---
+++ ember/app/controllers/application.js
@@ -0,0 +1,4 @@
+import Ember from 'ember';
+import SessionMixin from '../mixins/session';
+export default Ember.Controller.extend(SessionMixin, {
+});
--- ember/app/mixins/session.js
+++ ember/app/mixins/session.js
@@ -1,4 +1,12 @@
 import Ember from "ember";
 
 export default Ember.Mixin.create({
+	//computed properties
+  currentUser: function() {
+  	if (window.localStorage.getItem('session')) {
+    	return JSON.parse(window.localStorage.getItem('session'));
+    } else {
+    	return null;
+    }
+  }.property()
 });
