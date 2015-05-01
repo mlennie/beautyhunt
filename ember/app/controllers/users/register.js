72821836d7a1a8e171edf58a60dbb09a35fe598b
Monty Lennie <montylennie@gmail.com>
2015-05-01 15:10:50 +0200

    add Facebook connect button to register page

--- ember/app/controllers/users/register.js
+++ ember/app/controllers/users/register.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
-
-export default Ember.Controller.extend({
+import AuthenticationMixin from '../../mixins/authentication';
+export default Ember.Controller.extend(AuthenticationMixin,{
   //queryParams
 
   //properties
