e1afd67d50a1f4ccb3d5c469370e993a35a91dcb
Monty Lennie <montylennie@gmail.com>
2015-05-03 17:57:56 +0200

    refactor index and application styling

--- ember/app/controllers/index.js
+++ ember/app/controllers/index.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
-export default Ember.Controller.extend({
+import SessionMixin from '../mixins/session';
+export default Ember.Controller.extend(SessionMixin, {
 
 	//queryParams
 	queryParams: ['loginSuccess','alreadyLoggedIn', 'logoutSuccess'],
