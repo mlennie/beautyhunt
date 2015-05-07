8cfa996784c5b6b6530c67e8b9323043053e1dac
Monty Lennie <montylennie@gmail.com>
2015-05-08 01:49:02 +0200

    update styling

--- ember/app/controllers/item.js
+++ ember/app/controllers/item.js
@@ -5,12 +5,25 @@ export default Ember.ObjectController.extend({
 		var _this = this;
 		var itemTags = _this.store.all('item-tag');
 		var tags = [];
-		itemTags.forEach(function(element) {
+		itemTags.forEach(function(element, index) {
 			if (_this.get('model.id') == element.get('item_id')) {
 				var tag = _this.store.getById('tag', element.get('tag_id'));
 				tags.push(tag);
 			}
 		});
-		return tags;
+		var name; 
+		var names = [];
+		for(var i = 0; i < tags.length; i++) {
+			if (i < (tags.length - 1)) {
+				name = tags[i].get('name'); //+ ",";
+				console.log(name);
+			} else {
+				name = tags[i].get('name');
+				console.log(name);
+			}
+			names[i] = name;
+		}
+		console.log(names)
+		return names;
 	}.property()
 });
